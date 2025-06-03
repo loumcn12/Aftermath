a4b63b7ec257b704ec94efdc0d51684845f354e9
loumcn12 <loumcn12@kingshigh.school.nz>
2025-06-04 10:49:56 +1200

    Optimisation changes
    Removed _physics_process(delta) where possible and replaced with _process(delta), or migrated contents of _physics_process(delta) to an already existing _process(delta) where applicable.

---
+++ scripts/fps_label.gd
@@ -0,0 +1,24 @@
+extends Label
+
+var time_accumulator := 0.0
+const UPDATE_INTERVAL := 0.5
+
+func _ready() -> void:
+	_update_visibility()
+
+func _process(delta: float) -> void:
+
+	# Check if visibility changed during gameplay
+		_update_visibility()
+
+	if !Globalscript.showFPS:
+		return
+		
+	time_accumulator += delta
+	if time_accumulator >= UPDATE_INTERVAL:
+		text = "FPS: " + str(round(Engine.get_frames_per_second()))
+		time_accumulator = 0.0
+
+func _update_visibility() -> void:
+	visible = Globalscript.showFPS
+	set_process(visible)
