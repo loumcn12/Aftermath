5cd405a149fd9af83eb15d668f5f715ebbeab09f
loumcn12 <loumcn12@kingshigh.school.nz>
2025-06-11 23:01:42 +1200

    Fixed bug where buildings would not load in exported versions

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,17 +5,18 @@ extends Node3D
 @onready var player = $player
 var paused = false
 
-const BUILDINGS_PATH = "res://assets/buildings/"
-var TILE_SIZE = 20.0  # Set to 30.0 if that's your unit size
+# const BUILDINGS_PATH = "res://scenes/buildings/"
+# var TILE_SIZE = 20.0 (Not currently used)
 
-# You manually provide coordinates per size
+# Coordinates for each building size
 var tile_origins := {
-	"3x3": [Vector3(0, 0, -80), Vector3(0, 0, -120)],
-	"3x2": [Vector3(-64, 0, 0), Vector3(-64, 0, -40),Vector3(-64, 0 ,-80), Vector3(-64, 0 ,-120)],
-	"3x1": [Vector3(-128, 0, 0), Vector3(-128, 0, -40), Vector3(-128, 0 ,-80), Vector3(-128, 0 ,-120)],
-	"2x2": [Vector3(64, 0, 0), Vector3(64, 0, -40), Vector3(64, 0 ,-80), Vector3(64, 0 ,-120)],
-	"2x1": [Vector3(128, 0, 0), Vector3(128, 0, -40), Vector3(128, 0 , -80), Vector3(128, 0 , -120)],
-	"1x1": [Vector3(192, 0, 0), Vector3(192, 0, -40), Vector3(192, 0, -80), Vector3(192, 0 , -120), Vector3(-192, 0 ,0), Vector3(-192, 0 , -40), Vector3(-192, 0 , -80), Vector3(-192, 0 , -120)]
+	"3x3": [Vector3(0, 0, -120)],
+	"3x2": [Vector3(-64, 0, 0), Vector3(-64, 0, -40), Vector3(-64, 0, -80), Vector3(-64, 0, -120)],
+	"3x1": [Vector3(-128, 0, 0), Vector3(-128, 0, -40), Vector3(-128, 0, -80), Vector3(-128, 0, -120)],
+	"2x2": [Vector3(64, 0, 0), Vector3(64, 0, -40), Vector3(64, 0, -80), Vector3(64, 0, -120)],
+	"2x1": [Vector3(128, 0, 0), Vector3(128, 0, -40), Vector3(128, 0, -80), Vector3(128, 0, -120)],
+	"1x1": [Vector3(192, 0, 0), Vector3(192, 0, -40), Vector3(192, 0, -80), Vector3(192, 0, -120),
+			Vector3(-192, 0, 0), Vector3(-192, 0, -40), Vector3(-192, 0, -80), Vector3(-192, 0, -120)]
 }
 
 var building_scenes := {}  # Dictionary: "2x1" -> [PackedScene]
@@ -27,30 +28,49 @@ func _ready() -> void:
 	paused = false
 	load_building_models()
 	place_buildings()
-
+	
 func load_building_models():
-	var dir = DirAccess.open(BUILDINGS_PATH)
-	if dir == null:
-		push_error("Failed to open building directory.")
-		return
+	var building_files = [
+		"res://scenes/buildings/1x1_apartments.tscn",
+		"res://scenes/buildings/1x1_bunker.tscn",
+		"res://scenes/buildings/1x1_hanger.tscn",
+		"res://scenes/buildings/1x1_hospital.tscn",
+		"res://scenes/buildings/1x1_skyscraper.tscn",
+		"res://scenes/buildings/1x1_statue.tscn",
+		"res://scenes/buildings/1x1_tower.tscn",
+		"res://scenes/buildings/1x1_waccas.tscn",
+		"res://scenes/buildings/1x1_water.tscn",
+		"res://scenes/buildings/2x1_buildings.tscn",
+		"res://scenes/buildings/2x1_buildings.tscn",
+		"res://scenes/buildings/2x1_hospital.tscn",
+		"res://scenes/buildings/2x1_PizzaHut.tscn",
+		"res://scenes/buildings/2x1_School.tscn",
+		"res://scenes/buildings/2x2_Hospital.tscn",
+		"res://scenes/buildings/2x2_ParkingGarage.tscn",
+		"res://scenes/buildings/2x2_ParkingLot.tscn",
+		"res://scenes/buildings/2x2_Supermarket.tscn",
+		"res://scenes/buildings/3x1_Apartments.tscn",
+		"res://scenes/buildings/3x1_Buildings.tscn",
+		"res://scenes/buildings/3x1_Buildings_2.tscn",
+		"res://scenes/buildings/3x1_Buildings_3.tscn",
+		"res://scenes/buildings/3x2_Museum.tscn",
+		"res://scenes/buildings/3x2_Runway.tscn",
+		"res://scenes/buildings/3x3_Nuclear.tscn",
+		# Add all your building files here manually
+	]
 
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		if file_name.ends_with(".glb"):
-			var size_key = file_name.split("_")[0]  # e.g. "2x1" from "2x1_factory.glb"
+	for path in building_files:
+		var size_key = path.get_file().split("_")[0]
+		var scene = load(path)
+		if scene:
 			if not building_scenes.has(size_key):
 				building_scenes[size_key] = []
-			var scene = load(BUILDINGS_PATH + file_name)
-			if scene:
-				building_scenes[size_key].append(scene)
-		file_name = dir.get_next()
-	dir.list_dir_end()
+			building_scenes[size_key].append(scene)
 
 func place_buildings():
 	for size_key in tile_origins.keys():
 		if not building_scenes.has(size_key):
-			continue  # No buildings of this size
+			continue
 		for origin in tile_origins[size_key]:
 			var building_scene = building_scenes[size_key].pick_random()
 			if building_scene:
@@ -64,12 +84,11 @@ func place_building(scene: PackedScene, origin: Vector3):
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("escape"):
 		pauseMenu()
-	
+
 	if player.position.y < -5:
 		player._Damage(player.position.y * -0.008)
-		
-func pauseMenu():
 
+func pauseMenu():
 	if paused:
 		pause_menu.hide()
 		get_tree().paused = false
@@ -79,6 +98,5 @@ func pauseMenu():
 		get_tree().paused = true
 		Input.set_mouse_mode(Globalscript.mousemode)
 		hud.visible = false
-	await get_tree().create_timer(0.1).timeout 
+	await get_tree().create_timer(0.1).timeout
 	paused = !paused
-	
