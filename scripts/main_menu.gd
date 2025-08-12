b8e6ec2aafab031bd4799d3d0ee217999e3747f0
loumcn12 <loumcn12@kingshigh.school.nz>
2025-08-12 14:34:59 +1200

    Added level 2 to menu/playability

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var quitbutton = $VBoxContainer/quitbutton
 @onready var buttonSound = $AudioStreamPlayer2D
 @onready var versionLabel = $VersionLabel
+var level
 
 func _ready():
 	Input.set_mouse_mode(Globalscript.mousemode)
@@ -12,9 +13,13 @@ func _ready():
 	versionLabel.text = "Version " + str(ProjectSettings.get_setting("application/config/version"))
 	
 func _on_animation_done():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	if level == 1:
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
+	elif level == 2:
+		get_tree().change_scene_to_file("res://scenes/level2.tscn")
 	
 func _on_startbutton_pressed() -> void:
+	level = 1
 	buttonSound.play()
 	$VBoxContainer.visible = false
 	# Ensure one-shot connection before playing
@@ -33,3 +38,14 @@ func _on_quitbutton_pressed() -> void:
 	buttonSound.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().quit()
+
+
+func _on_startbutton_2_pressed() -> void:
+	level = 2
+	buttonSound.play()
+	$VBoxContainer.visible = false
+	# Ensure one-shot connection before playing
+	menudoor.animation_done.connect(_on_animation_done, CONNECT_ONE_SHOT)
+	menudoor.get_node("AnimationPlayer").play("menu_door_open")
+	menu.get_node("WorldEnvironment").environment.volumetric_fog_enabled = true
+	menu.get_node("FogVolume").visible = true
